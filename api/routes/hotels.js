53c09fe30747b809aeb83b6407347d433cd44a89
tbs <tbson2000@gmail.com>
2022-07-20 14:37:38 +0700

    testing

---
+++ api/routes/hotels.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { createHotel, deleteHotel, getHotel, getHotels, updatedHotel } from "../controllers/hotel.js";
+import Hotel from "../models/Hotel.js";
+import {verifyAdmin} from "../utils/verifyToken.js"
+const router = express.Router();
+
+//CREATE
+router.post("/", verifyAdmin, createHotel);
+
+//UPDATE
+router.put("/:id",verifyAdmin, updatedHotel);
+//DELETE
+
+router.delete("/:id",verifyAdmin, deleteHotel);
+//GET
+
+router.get("/:id", getHotel);
+//GET ALL
+
+router.get("/", getHotels)
+
+export default router
