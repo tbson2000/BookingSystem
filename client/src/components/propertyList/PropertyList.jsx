e770d1f3e055c435920b7d31c46195adbc247549
Don14 <donnhat14@gmail.com>
2022-08-09 16:26:41 +0700

    Change font

--- client/src/components/propertyList/PropertyList.jsx
+++ client/src/components/propertyList/PropertyList.jsx
@@ -6,10 +6,10 @@ const PropertyList = () => {
   // console.log(data)
   const images = [
     "https://expertphotography.b-cdn.net/wp-content/uploads/2021/02/children-photographers-2-1.jpg",
-    "https://i.pinimg.com/originals/46/cd/e1/46cde1793ed0e7727ffbf6dc7a7ea189.jpg",
     "https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/home/6-1.jpg",
+    "https://e3.365dm.com/19/08/2048x1152/skynews-pewdiepie-marzi-bisognin_4751188.jpg",
     "https://img.buzzfeed.com/buzzfeed-static/static/2021-05/13/18/asset/b8c401eadbd6/sub-buzz-609-1620930319-24.jpg",
-    "https://res.cloudinary.com/yaffa-publishing/image/fetch/q_auto:best,c_fit,w_630,f_auto/http%3A%2F%2Fyaffa-cdn.s3.amazonaws.com%2Fyaffadsp%2Fimages%2FdmImage%2FSourceImage%2Fjavier-garciabpishutterstock-.jpg"
+    "https://i.pinimg.com/originals/46/cd/e1/46cde1793ed0e7727ffbf6dc7a7ea189.jpg",
   ]
   return (
     <div className="pList">
